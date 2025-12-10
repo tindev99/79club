2c122448a8719340e049796301d6f07d5978cffd
tindev99 <acc5cayncff@gmail.com>
2025-12-11 00:38:18 +0700

    .

--- main.891be.js
+++ main.891be.js
@@ -124,7 +124,7 @@ window.boot = function () {
     if ('requestIdleCallback' in window) {
         requestIdleCallback(startGame);
     } else {
-        setTimeout(startGame, 500);
+        setTimeout(startGame, 200);
     }
 };
 
